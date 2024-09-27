33d60d124b3282ed64535f675e31997e0b21c30c
PlagueFencer <plaguefencer@noleron.com>
2024-09-27 13:43:06 +0300

    Save

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import pygame
+
+pygame.init()
+
+HEIGHT = 800
+WIDTH = 1200
+
+main_display = pygame.display.set_mode((HEIGHT, WIDTH))
+
+count = 0
+while True:
+    print("Hello world!")
+    count += 1
+    if count >= 10:
+        break
